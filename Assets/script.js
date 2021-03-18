e1f60ae2a9fb0ad6239926968bb9e91411cf21a8
Steve LeValley <slevalley@gmail.com>
2021-03-17 22:07:25 -0700

    Now has password length and type of character prompts

--- Assets/script.js
+++ Assets/script.js
@@ -3,7 +3,14 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", 
                 "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase = String.prototype.toUpperCase.apply(lowercase).split(",");
-console.log(uppercase);
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var characters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", 
+                    "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", "<", ",", ".", ">",
+                    "/", "?"]
+
+
+
+
 
 
 // Write password to the #password input
@@ -23,10 +30,43 @@ function generatePassword() {
     var pwLength = prompt("How many characters would you like your password to be?");
 
     if (pwLength >= 8 && pwLength <= 128) {
-        console.log ("Good Length");
+        var confirmLowercase = confirm("Click OK to confirm including LOWERCASE letters");
+        var confirmUppercase = confirm("Click OK to confirm including UPPERCASE letters");
+        var confirmNumbers = confirm("Click OK to confirm including NUMBERS");
+        var confirmSpecial = confirm("Click OK to confirm including SPECIAL CHARACTERS");
+        if (!confirmLowercase && !confirmUppercase && !confirmNumbers && !confirmSpecial) {
+            window.alert("You must select at least one category!");
+            generatePassword();
+        }
+        
+
+    } else if (pwLength !== null) {
+        window.alert("You must enter a number between 8 and 128");
+        generatePassword();
     } else {
-        console.log ("No Good");
+        return;
+    };
+
+    if (confirmNumbers === null) {
+        window.alert("Hello!");
     }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 };
