72a93b0773814578de954d51a27bed308537dc2b
Steve LeValley <slevalley@gmail.com>
2021-03-19 08:49:52 -0700

    Complete besides README

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 
 
 
-// Assignment Code
+// GLOBAL Variables
 var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", 
                 "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -14,9 +14,13 @@ var special = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", 
 var userArray = [];
 var UserPW = [];
 
-
-// Add event listener to generate button
+// Add Event Listener to Generate Button
 generateBtn.addEventListener("click", writePassword);
+
+
+//###############Whole Write Password function
+//###############Whole Write Password function
+//###############Whole Write Password function
 
 function writePassword() {
       var password = generatePassword();
@@ -26,9 +30,10 @@ function writePassword() {
       passwordText.value = password; 
     }
 
+// Function that Generates Password 
 function generatePassword() {
     
-    
+    // Initial prompt for user to select password length, requiring them to choose a password between 8 - 128 characters
     var pwLength = prompt("How many characters would you like your password to be?");
     var UserPW = [];
 
@@ -50,28 +55,44 @@ function generatePassword() {
         return;
     };
 
-    
+    // Establishing variables to be used to randomly add 1 item from each of the 4 types of characters if 
+    // the user wishes for that type to be included
+    var lowerrandomIndex = (Math.floor(Math.random()*26));
+    var upperrandomIndex = (Math.floor(Math.random()*26));
+    var numberrandomIndex = (Math.floor(Math.random()*10));
+    var specialrandomIndex = (Math.floor(Math.random()*30));
 
+    // If "true", random item from that array is added to user password and concatinating the arrays of 
+    // each type of character the user chose into one larger array called "userArray"
     if (confirmLowercase === true) {
         userArray = userArray.concat(lowercase);
+        UserPW.push(lowercase[lowerrandomIndex]);
+        console.log(UserPW);
     };
     
     if (confirmUppercase === true) {
         userArray = userArray.concat(uppercase);
+        UserPW.push(uppercase[upperrandomIndex]);
+        console.log(UserPW);
     };
 
     if (confirmNumbers === true) {
         userArray = userArray.concat(numbers);
+        UserPW.push(numbers[numberrandomIndex]);
+        console.log(UserPW);
     };
 
     if (confirmSpecial === true) {
         userArray = userArray.concat(special);
+        UserPW.push(special[specialrandomIndex]);
+        console.log(UserPW);
     };
 
 
-    
+    // Grabbing random number item from "userArray" and adding it to the final "UserPW" array and returning 
+    // the final "UserPW" array to the function.
 
-    for (i=0; i < pwLength; i++) {
+    for (i=UserPW.length; i < pwLength; i++) {
         UserPW[i] = userArray[Math.floor(Math.random()* userArray.length)];
     }    
     return UserPW.join('');
--- README.md
+++ README.md
@@ -15,20 +15,3 @@ WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
 
 
-Variables - 
-    - 
-
-5. When the button is clicked on, the user is prompted how many characters they would like the password to be.
-
-
-6. If the user input is not a number, or less than 8, or greater than 128, tell the user that it must be a number between 8 and 128.
-6. Reprompt user to input a number between 8 and 128.
-7. If the user inputs a number between 8 and 128, 
-    - ask if the user would like to include lowercase letters
-    - ask if the user would like to include uppercase letters
-    - ask if the user would like to include numbers
-    - ask if the user would like to include special characters
-
-8. If the user would like to use all 4, use randomly selected from Array that contains all 4 types
-
-9. If user would like three types, use randomly selected
